d2fdb261f1adda1d2d5cfc6b052a32094ae1cd4b
Jonathon Hibbard <jhibbard@gmail.com>
2013-07-16 22:14:29 -0400

    Added StaticNav to the toolset.  This is currently UNTESTED code!  You've been warned.

---
+++ classes/Toolkit/StaticNav.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * @author Jonathon Hibbard, 2013
+ *
+ * This is a simple object for working with a site's navigation system.
+ * It is not intended to be anything less than a temporary toolkit object for instances
+ * where there is a need to simply satisfy navigation functionality on the site (programaticallly).
+ *
+ * Built this for my new portfolio page just as a quick way to offer a temporary solution until I
+ * can integrate a router/controller system.
+ */
+
+namespace Toolkit;
+class StaticNav {
+
+  public static $instance = null;
+
+  private $pages = array();
+  private $currentPage = null;
+
+  private function __construct() {
+
+    $this->pages = array("intro"     => "/",
+                         "portfolio" => "/",
+                         "resume"    => "http://phpadvocate.com/blog/resume/",
+                         "blog"      => "http://www.phpadvocate.com",
+                         "github"    => "https://github.com/infolock",
+                         "contact"   => "#");
+  }
+
+  public function getActivePage() {
+    return ($this->currentPage != null ? $this->getPageInfoForPage($this->currentPage) : null);
+  }
+
+  public function setActivePage($page) {
+    if(is_string($page) && isset($this->pages[strtolower($page)])) {
+      $this->currentPage = strtolower($page);
+      return true;
+    }
+    return false;
+  }
+
+  public function getPageInfoForPage($page) {
+    if(is_string($page) && isset($this->pages[strtolower($page)])) {
+      $page = strotlower($page);
+      return array("name" => $page,
+                   "url"  => $this->pages[$page]
+                  );
+    }
+    return false;
+  }
+
+
+  public static function getInstance() {
+    if(self::$instance === null) {
+      self::$instance = new StaticNav();
+    }
+    return self::$instance;
+  }
+
+  public static function currentPage() {
+    return self::getInstance()->getCurrentPage();
+  }
+
+  public static function setCurrentPage() {
+    return self::getInstance()->getCurrentPage();
+  }
+}
+?>
